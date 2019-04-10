4cebe2a2cb8971d7b180c0ff56e58bbee4dc22a2
Matan Gover <matangover@gmail.com>
2019-04-10 14:19:47 -0400

    Use mypy analysis engine.

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,27 +5,17 @@
 */
 
 import {
-    createConnection, Diagnostic, DiagnosticSeverity, IConnection,
+    createConnection, IConnection,
     InitializeResult, IPCMessageReader, IPCMessageWriter, Location,
-    MarkupContent, Position, Range, TextDocuments
+    Position, Range, TextDocuments
 } from 'vscode-languageserver';
 
-import { AnalyzerService } from './analyzer/service';
-import { CommandLineOptions } from './common/commandLineOptions';
-import { Diagnostic as AnalyzerDiagnostic, DiagnosticCategory, DiagnosticTextPosition,
-    DiagnosticTextRange } from './common/diagnostic';
-import { combinePaths, getDirectoryPath, normalizePath } from './common/pathUtils';
+import { exec, ExecException } from 'child_process';
+import { getDirectoryPath } from './common/pathUtils';
 
-interface PythonSettings {
-    venvPath?: string;
-    analysis?: {
-        typeshedPaths: string[];
-    };
-}
-
-interface Settings {
-    python: PythonSettings;
-}
+// interface Settings {
+//     python: PythonSettings;
+// }
 
 // Stash the base directory into a global variable.
 (global as any).__rootDirectory = getDirectoryPath(__dirname);
@@ -39,57 +29,63 @@ _connection.console.log('Pyright language server starting');
 // supports full document sync only.
 let _documents: TextDocuments = new TextDocuments();
 
-// Allocate the analyzer service instance.
-let _analyzerService: AnalyzerService = new AnalyzerService(_connection.console);
-
-// Root path of the workspace.
-let _rootPath = '/';
-
 // Tracks whether we're currently displaying progress.
-let _isDisplayingProgress = false;
+// let _isDisplayingProgress = false;
 
 // Make the text document manager listen on the connection
 // for open, change and close text document events.
 _documents.listen(_connection);
 
+const dmypy = '/Users/matan/Documents/code/mypy/.venv38/bin/dmypy';
+
+let started = false;
+let queuedAnalyses: string[] = [];
+
 // After the server has started the client sends an initialize request. The server receives
 // in the passed params the rootPath of the workspace plus the client capabilities.
 _connection.onInitialize((params): InitializeResult => {
-    _rootPath = params.rootPath || '/';
 
     // Don't allow the analysis engine to go too long without
     // reporting results. This will keep it responsive.
-    _analyzerService.setMaxAnalysisDuration({
-        openFilesTimeInMs: 50,
-        noOpenFilesTimeInMs: 1000
-    });
+    // _analyzerService.setMaxAnalysisDuration({
+    //     openFilesTimeInMs: 50,
+    //     noOpenFilesTimeInMs: 1000
+    // });
 
-    _analyzerService.setCompletionCallback(results => {
-        results.diagnostics.forEach(fileDiag => {
-            let diagnostics = _convertDiagnostics(fileDiag.diagnostics);
+    // _analyzerService.setCompletionCallback(results => {
+    //     results.diagnostics.forEach(fileDiag => {
+    //         let diagnostics = _convertDiagnostics(fileDiag.diagnostics);
 
-            // Send the computed diagnostics to the client.
-            _connection.sendDiagnostics({
-                uri: _convertPathToUri(fileDiag.filePath),
-                diagnostics
-            });
+    //         // Send the computed diagnostics to the client.
+    //         _connection.sendDiagnostics({
+    //             uri: _convertPathToUri(fileDiag.filePath),
+    //             diagnostics
+    //         });
 
-            if (results.filesRequiringAnalysis > 0) {
-                if (!_isDisplayingProgress) {
-                    _isDisplayingProgress = true;
-                    _connection.sendNotification('pyright/beginProgress');
-                }
+    //         if (results.filesRequiringAnalysis > 0) {
+    //             if (!_isDisplayingProgress) {
+    //                 _isDisplayingProgress = true;
+    //                 _connection.sendNotification('pyright/beginProgress');
+    //             }
 
-                const fileOrFiles = results.filesRequiringAnalysis !== 1 ? 'files' : 'file';
-                _connection.sendNotification('pyright/reportProgress',
-                    `${ results.filesRequiringAnalysis } ${ fileOrFiles } to analyze`);
-            } else {
-                if (_isDisplayingProgress) {
-                    _isDisplayingProgress = false;
-                    _connection.sendNotification('pyright/endProgress');
-                }
-            }
-        });
+    //             const fileOrFiles = results.filesRequiringAnalysis !== 1 ? 'files' : 'file';
+    //             _connection.sendNotification('pyright/reportProgress',
+    //                 `${ results.filesRequiringAnalysis } ${ fileOrFiles } to analyze`);
+    //         } else {
+    //             if (_isDisplayingProgress) {
+    //                 _isDisplayingProgress = false;
+    //                 _connection.sendNotification('pyright/endProgress');
+    //             }
+    //         }
+    //     });
+    // });
+    _connection.console.log(`Running dmypy in ${process.cwd()}`);
+    execLog(`${dmypy} restart --log-file dmypy.log -- --use-fine-grained-cache --follow-imports=skip`, (error, stdout, stderr) => {
+        started = true;
+        for (const queued of queuedAnalyses) {
+            analyze(queued);
+        }
+        queuedAnalyses = [];
     });
 
     return {
@@ -97,141 +93,171 @@ _connection.onInitialize((params): InitializeResult => {
             // Tell the client that the server works in FULL text document
             // sync mode (as opposed to incremental).
             textDocumentSync: _documents.syncKind,
-            definitionProvider: true,
-            hoverProvider: true
+            definitionProvider: true
+            // hoverProvider: true
         }
     };
 });
 
 // The content of a text document has changed. This event is emitted
 // when the text document first opened or when its content has changed.
-_documents.onDidChangeContent(change => {
+_documents.onDidSave(change => {
+    // TODO: not actually called
     let filePath = _convertUriToPath(change.document.uri);
-    _connection.console.log(`File "${ filePath }" changed -- marking dirty`);
-    _analyzerService.markFilesChanged([filePath]);
-    updateOptionsAndRestartService();
+    _connection.console.log(`File "${ filePath }" saved -- checking`);
+    queueAnalyze(filePath);
+    // _analyzerService.markFilesChanged([filePath]);
+    // updateOptionsAndRestartService();
+});
+// _connection.onDidChangeConfiguration(change => {
+    // _connection.console.log(`Received updated settings.`);
+    // updateOptionsAndRestartService(change.settings);
+// });
+
+_connection.onDidSaveTextDocument(change => {
+    let filePath = _convertUriToPath(change.textDocument.uri);
+    _connection.console.log(`File "${ filePath }" saved [conn] -- checking`);
+    queueAnalyze(filePath);
 });
 
-_connection.onDidChangeConfiguration(change => {
-    _connection.console.log(`Received updated settings.`);
-    updateOptionsAndRestartService(change.settings);
-});
-
-_connection.onDefinition(params => {
+_connection.onDefinition(async params => {
     let filePath = _convertUriToPath(params.textDocument.uri);
+    const stdout = await new Promise<string>(resolve => {
+        execLog(`${dmypy} suggest --callsites "${filePath} ${params.position.line + 1} ${params.position.character + 1}"`,
+            (_error, stdout, _stderr) => {
+            resolve(stdout);
+        });
+    });
+    // let position: DiagnosticTextPosition = {
+    //     line: params.position.line,
+    //     column: params.position.character
+    // };
 
-    let position: DiagnosticTextPosition = {
-        line: params.position.line,
-        column: params.position.character
-    };
-
-    let location = _analyzerService.getDefinitionForPosition(filePath, position);
-    if (!location) {
-        return undefined;
+    // let location = _analyzerService.getDefinitionForPosition(filePath, position);
+    // if (!location) {
+    //     return undefined;
+    // }
+    // return Location.create(_convertPathToUri(location.path), _convertRange(location.range));
+    const result = /at (.+):([0-9]+):([0-9]+)/.exec(stdout);
+    if (!result) {
+        return null;
     }
-    return Location.create(_convertPathToUri(location.path), _convertRange(location.range));
+    _connection.console.log(`Found definition at ${result[1]}:${result[2]}:${result[3]}`);
+    const position = Position.create(parseInt(result[2], 10) - 1, parseInt(result[3], 10) - 1);
+    return Location.create(_convertPathToUri(result[1]), Range.create(position, position));
 });
 
-_connection.onHover(params => {
-    let filePath = _convertUriToPath(params.textDocument.uri);
-
-    let position: DiagnosticTextPosition = {
-        line: params.position.line,
-        column: params.position.character
-    };
-
-    let hoverMarkdown = _analyzerService.getHoverForPosition(filePath, position);
-    if (!hoverMarkdown) {
-        return undefined;
+function queueAnalyze(filePath: string) {
+    if (started) {
+        analyze(filePath);
+    } else {
+        queuedAnalyses.push(filePath);
     }
-    let markupContent: MarkupContent = {
-        kind: 'markdown',
-        value: hoverMarkdown
-    };
-    return { contents: markupContent };
-});
-
-function updateOptionsAndRestartService(settings?: Settings) {
-    let commandLineOptions = new CommandLineOptions(_rootPath, true);
-    commandLineOptions.watch = true;
-    if (settings && settings.python) {
-        if (settings.python.venvPath) {
-            commandLineOptions.venvPath = combinePaths(_rootPath,
-                normalizePath(_expandPathVariables(settings.python.venvPath)));
-        }
-        if (settings.python.analysis &&
-                settings.python.analysis.typeshedPaths &&
-                settings.python.analysis.typeshedPaths.length > 0) {
-
-            // Pyright supports only one typeshed path currently, whereas the
-            // official VS Code Python extension supports multiple typeshed paths.
-            // We'll use the first one specified and ignore the rest.
-            commandLineOptions.typeshedPath =
-                _expandPathVariables(settings.python.analysis.typeshedPaths[0]);
-        }
-    }
-
-    _analyzerService.setOptions(commandLineOptions);
 }
+
+function analyze(filePath: string) {
+    _connection.console.log(`Analyzing "${ filePath }"`);
+    execLog(`${dmypy} check -- ${filePath}`);
+}
+
+// _connection.onHover(params => {
+//     let filePath = _convertUriToPath(params.textDocument.uri);
+
+//     let position: DiagnosticTextPosition = {
+//         line: params.position.line,
+//         column: params.position.character
+//     };
+
+//     let hoverMarkdown = _analyzerService.getHoverForPosition(filePath, position);
+//     if (!hoverMarkdown) {
+//         return undefined;
+//     }
+//     let markupContent: MarkupContent = {
+//         kind: 'markdown',
+//         value: hoverMarkdown
+//     };
+//     return { contents: markupContent };
+// });
+
+// function updateOptionsAndRestartService(settings?: Settings) {
+//     let commandLineOptions = new CommandLineOptions(_rootPath, true);
+//     commandLineOptions.watch = true;
+//     if (settings && settings.python) {
+//         if (settings.python.venvPath) {
+//             commandLineOptions.venvPath = combinePaths(_rootPath,
+//                 normalizePath(_expandPathVariables(settings.python.venvPath)));
+//         }
+//         if (settings.python.analysis &&
+//                 settings.python.analysis.typeshedPaths &&
+//                 settings.python.analysis.typeshedPaths.length > 0) {
+
+//             // Pyright supports only one typeshed path currently, whereas the
+//             // official VS Code Python extension supports multiple typeshed paths.
+//             // We'll use the first one specified and ignore the rest.
+//             commandLineOptions.typeshedPath =
+//                 _expandPathVariables(settings.python.analysis.typeshedPaths[0]);
+//         }
+//     }
+
+//     _analyzerService.setOptions(commandLineOptions);
+// }
 
 // Expands certain predefined variables supported within VS Code settings.
 // Ideally, VS Code would provide an API for doing this expansion, but
 // it doesn't. We'll handle the most common variables here as a convenience.
-function _expandPathVariables(value: string): string {
-    const regexp = /\$\{(.*?)\}/g;
-    return value.replace(regexp, (match: string, name: string) => {
-        const trimmedName = name.trim();
-        if (trimmedName === 'workspaceFolder') {
-            return _rootPath;
-        }
-        return match;
-    });
-}
+// function _expandPathVariables(value: string): string {
+//     const regexp = /\$\{(.*?)\}/g;
+//     return value.replace(regexp, (match: string, name: string) => {
+//         const trimmedName = name.trim();
+//         if (trimmedName === 'workspaceFolder') {
+//             return _rootPath;
+//         }
+//         return match;
+//     });
+// }
 
-function _convertDiagnostics(diags: AnalyzerDiagnostic[]): Diagnostic[] {
-    return diags.map(diag => {
-        let severity = diag.category === DiagnosticCategory.Error ?
-            DiagnosticSeverity.Error : DiagnosticSeverity.Warning;
+// function _convertDiagnostics(diags: AnalyzerDiagnostic[]): Diagnostic[] {
+//     return diags.map(diag => {
+//         let severity = diag.category === DiagnosticCategory.Error ?
+//             DiagnosticSeverity.Error : DiagnosticSeverity.Warning;
 
-        return Diagnostic.create(_convertRange(diag.range), diag.message, severity,
-            undefined, 'pyright');
-    });
-}
+//         return Diagnostic.create(_convertRange(diag.range), diag.message, severity,
+//             undefined, 'pyright');
+//     });
+// }
 
-function _convertRange(range?: DiagnosticTextRange): Range {
-    if (!range) {
-        return Range.create(_convertPosition(), _convertPosition());
-    }
-    return Range.create(_convertPosition(range.start), _convertPosition(range.end));
-}
+// function _convertRange(range?: DiagnosticTextRange): Range {
+//     if (!range) {
+//         return Range.create(_convertPosition(), _convertPosition());
+//     }
+//     return Range.create(_convertPosition(range.start), _convertPosition(range.end));
+// }
 
-function _convertPosition(position?: DiagnosticTextPosition): Position {
-    if (!position) {
-        return Position.create(0, 0);
-    }
-    return Position.create(position.line, position.column);
-}
+// function _convertPosition(position?: DiagnosticTextPosition): Position {
+//     if (!position) {
+//         return Position.create(0, 0);
+//     }
+//     return Position.create(position.line, position.column);
+// }
 
 _connection.onDidOpenTextDocument(params => {
-    let filePath = _convertUriToPath(params.textDocument.uri);
-    _analyzerService.setFileOpened(
-        filePath,
-        params.textDocument.version,
-        params.textDocument.text);
+    const filePath = _convertUriToPath(params.textDocument.uri);
+    _connection.console.log(`File "${filePath}" opened -- checking`);
+    queueAnalyze(filePath);
 });
 
-_connection.onDidChangeTextDocument(params => {
-    let filePath = _convertUriToPath(params.textDocument.uri);
-    _analyzerService.updateOpenFileContents(
-        filePath,
-        params.textDocument.version,
-        params.contentChanges[0].text);
-});
+// _connection.onDidChangeTextDocument(params => {
+//     let filePath = _convertUriToPath(params.textDocument.uri);
+//     _analyzerService.updateOpenFileContents(
+//         filePath,
+//         params.textDocument.version,
+//         params.contentChanges[0].text);
+// });
 
-_connection.onDidCloseTextDocument(params => {
-    let filePath = _convertUriToPath(params.textDocument.uri);
-    _analyzerService.setFileClosed(filePath);
-});
+// _connection.onDidCloseTextDocument(params => {
+//     let filePath = _convertUriToPath(params.textDocument.uri);
+//     _analyzerService.setFileClosed(filePath);
+// });
 
 function _convertUriToPath(uri: string): string {
     const fileScheme = 'file://';
@@ -248,3 +274,37 @@ function _convertPathToUri(path: string): string {
 
 // Listen on the connection
 _connection.listen();
+
+async function execAsync(command: string): Promise<string> {
+    return new Promise<string>(resolve => {
+        _connection.console.log(`Execute: ${command}`);
+        exec(command, (error, stdout, stderr) => {
+            _connection.console.log(`command finished, result: ${error ? `failed (${error.code})` : 'success'}`);
+            if (stdout) {
+                _connection.console.log(`stdout:\n${stdout}`);
+            }
+            if (stderr) {
+                _connection.console.log(`stderr:\n${stderr}`);
+            }
+            resolve(stdout);
+        });
+    });
+}
+
+function execLog(command: string, callback?: (error: ExecException | null, stdout: string, stderr: string) => void) {
+    _connection.console.log(`Execute: ${command}`);
+    _connection.console.log(`cwd: ${process.cwd()}`);
+    exec(command, (error, stdout, stderr) => {
+        _connection.console.log(`command finished, result: ${error ? `failed (${error.code})` : 'success'}`);
+        if (stdout) {
+            _connection.console.log(`stdout:\n${stdout}`);
+        }
+        if (stderr) {
+            _connection.console.log(`stderr:\n${stderr}`);
+        }
+
+        if (callback) {
+            callback(error, stdout, stderr);
+        }
+    });
+}
